5a7f75b6cd8bcb26b2121729ff41b8d23522919f
donjevas <99785536+donjevas@users.noreply.github.com>
2023-01-12 20:25:15 -0600

    Update main.tf
    made an error on sg put 1 instead of 2

--- main.tf
+++ main.tf
@@ -8,7 +8,7 @@ module "ec2_instance" {
   instance_type          = "t2.micro"
   key_name               = "user1"
   monitoring             = true
-  vpc_security_group_ids = ["sg-12345678"]
+  vpc_security_group_ids = ["sg-22345678"]
   subnet_id              = "subnet-eddcdzz4"
 
   tags = {
